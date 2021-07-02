c0573e5286bad0bd004549685489b9bec96e8e9f
Mitchell <Mitchell@Family-Mac-mini-2.local>
2021-07-02 15:41:05 -0500

    Created FilmCard and PersonCard files.

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useState } from 'react';
 import Header from './components/Header';
+import FilmCard from './components/FilmCard';
+import PersonCard from './components/PersonCard';
 
 const App = () => {
     // use states
@@ -64,69 +66,11 @@ const App = () => {
             </div>
             {/* Film Cards */}
             <div id="generated-films" className="row d-flex justify-content-center">
-                {films.map(film => (
-                    <div className="card col-md-3 m-3" key={`film-card-${film.title}`}>
-                        <div className="card-body">
-                            <h3 className="card-title text-center">{film.title}</h3>
-                            <h6 className="card-subtitle text-muted text-center">{film.original_title}</h6>
-                            <h6 className="card-subtitle text-muted text-center mb-4">{film.original_title_romanised}</h6>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Director: </span>
-                                    <span>{film.director}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Producer: </span>
-                                    <span>{film.producer}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Release Date: </span>
-                                    <span>{film.release_date}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Running Time: </span>
-                                    <span>{film.running_time} minutes</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Rotten Tomato Score: </span>
-                                    <span>{film.rt_score}%</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Description: </span>
-                                    <span>{film.description}</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                ))}
+            {films.map(film => <FilmCard film={film} />)}
             </div>
             {/* People Cards */}
             <div id="generated-people" className="row d-flex justify-content-center">
-                {people.map(person => (
-                    <div className="card col-md-3 m-3" key={`person-card-${person.name}`}>
-                        <div className="card-body">
-                            <h3 className="card-title text-center mb-4">{person.name}</h3>
-                            <ul className="list-group list-group-flush">
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Gender: </span>
-                                    <span>{person.gender}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Age: </span>
-                                    <span>{person.age}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Eye Color: </span>
-                                    <span>{person.eye_color}</span>
-                                </li>
-                                <li className="list-group-item">
-                                    <span style={{ color: "#2c9be5" }}>Hair Color: </span>
-                                    <span>{person.hair_color}</span>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                ))}
+                {people.map(person => <PersonCard person={person} />)}
             </div>
 
         </>
